579e2358e16fdf070474962eebd2889e6fcf67eb
amd <amd@backport-euler510.amd.com>
2025-06-05 12:25:37 +0530

    Updating Check_deps script
    Adding -l option in that script to process multiple input at a time

--- Check_deps/checkdep.py
+++ Check_deps/checkdep.py
@@ -9,40 +9,55 @@ import concurrent.futures
 # Define a function to check each commit in parallel
 def check_commit(cmid):
     short_cmid = cmid[:12]
-#    if short_cmid not in stable_log:
-#        return f"commit {short_cmid} not available"
 
     # Check for dependencies using `--grep`
     deps_result = subprocess.run(f"git log origin/stable_kernel_6.13 --oneline --grep=\"Fixes: {short_cmid}\"", stdout=subprocess.PIPE, shell=True, universal_newlines=True)
     deps_output = deps_result.stdout.strip() or "no deps"
     return f"==>checking {short_cmid}\n{deps_output}"
 
+# function to process commit ids if not given as input 
+def Process_upstream(up_cmids,count):
+    log_out = subprocess.run(f"git log -{count} ", stdout=subprocess.PIPE, shell=True, universal_newlines=True)
+    lines = [line for line in log_out.stdout.splitlines()]
+
+    pattern = rf"\b{re.escape('commit')}\s+(\w+)\s+{re.escape('upstream')}\b"
+
+    for line in lines:
+        if re.search(pattern, line):
+            up_cmids.append(line.strip().split()[1])
+
+#Function to process commit ids given as input
+def getlist(input_list):
+    print("Enter a value (or type 'done' to finish): ")
+    while True:
+        user_input = input()
+        if user_input.lower() == 'done':
+             break
+        input_list.append(user_input)
+
+#--MAIN--
 # check the arguments
 if len(sys.argv) < 2:
-    print("Enter the count or commit id to check the deps")
+    print("Enter the count(inside Linux_Backport repo) or -l(to add a list of commit ids) to check the deps")
     sys.exit(0)
+
+up_cmids = []
+count=0
 
 try:
     ret_val = int(sys.argv[1])
     count = sys.argv[1]
 except ValueError:
-    print(check_commit(sys.argv[1]))
-    sys.exit(0)
+    if sys.argv[1] == "-l":
+        getlist(up_cmids)
+    else :
+        print("Invalid input")
+        sys.exit(1)
 
-log_out = subprocess.run(f"git log -{count} ", stdout=subprocess.PIPE, shell=True, universal_newlines=True)
-lines = [line for line in log_out.stdout.splitlines()]
+if count != 0 :
+    Process_upstream(up_cmids,count)
 
-pattern = rf"\b{re.escape('commit')}\s+(\w+)\s+{re.escape('upstream')}\b"
-up_cmids = []
-
-for line in lines:
-    if re.search(pattern, line):
-        up_cmids.append(line.strip().split()[1])
-#print([x for x in up_cmids])
-
-#result = subprocess.run(f"git log origin/stable_kernel_6.11 --oneline", stdout=subprocess.PIPE, shell=True, universal_newlines=True, errors='ignore')
-#stable_log = result.stdout.splitlines()
-
+print(up_cmids)
 # Use a ThreadPoolExecutor for parallel processing
 with concurrent.futures.ThreadPoolExecutor() as executor:
     results = list(executor.map(check_commit, up_cmids))
@@ -50,4 +65,3 @@ with concurrent.futures.ThreadPoolExecutor() as executor:
 # Print all results
 for result in results:
     print(result)
-
